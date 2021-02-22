37d857152072619499181cb582f81b76e7d214c3
Jiefeng Kang <kangjf1943@gmail.com>
2021-02-22 21:21:37 +0900

    Extract pdf to text
    Batch operation with loop structure to get content from policy pdf files, and write the content to text files.

--- Data_analysis.R
+++ Data_analysis.R
@@ -2,27 +2,29 @@ library(RMeCab)
 library(pdftools)
 library(wordcloud)
 
-Sys.setlocale(category = "LC_ALL", locale = "Japanese")
-policy_text <- pdf_text("Policy_text/京都市.pdf")
-policy_text[1]
-# 文本显示为日语
-nchar(policy_text)
-# 共两项，一千多字
+setwd("C:/Users/kangj/Documents/R/BDP/Policy_pdf")
+for (i in list.files("C:/Users/kangj/Documents/R/BDP/Policy_pdf")) {
+  Sys.setlocale(category = "LC_ALL", locale = "Japanese")
+  policy_text <- pdf_text(i)
+  # 文本显示为日语吗？
+  nchar(policy_text)
+  
+  # 文本处理
+  class(policy_text)
+  length(policy_text)
+  # 删除特殊字符
+  policy_text <- gsub("\r", "", policy_text)
+  policy_text <- gsub("\n", "", policy_text)
+  policy_text <- gsub("@", "", policy_text)
+  policy_text
+  # 删除数字
+  
+  # 日语分词怎么做？
+  # 消除停止词之后直接分析词频吧
+  # 先写入该文档
+  write(policy_text, paste("C:/Users/kangj/Documents/R/BDP/Policy_text/",i,".txt", sep = ""))
+}
 
-# 文本处理
-class(policy_text)
-length(policy_text)
-# 删除特殊字符
-policy_text <- gsub("\r", "", policy_text)
-policy_text <- gsub("\n", "", policy_text)
-policy_text <- gsub("@", "", policy_text)
-policy_text
-# 删除数字
-
-# 日语分词怎么做？
-# 消除停止词之后直接分析词频吧
-# 先写入该文档
-write(policy_text, "Policy.txt")
 # 写入的文档是ANSI编码，但直接用RMecab分析的话有时会显示乱码？
 policy_freq <- RMeCabFreq("Policy.txt")
 policy_freq
