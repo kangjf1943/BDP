5d471fe4864c616d42966d9988eb6acf17e77f03
Jiefeng Kang <kangjf1943@gmail.com>
2021-02-22 18:45:43 +0900

    Analysis of single policy document

--- Policy_extract.R
+++ Policy_extract.R
@@ -1,32 +1,15 @@
-# practice
-library(pdftools)
-library(tidyverse)
-library(jiebaR)
-
-policy_text <- pdf_text("Policy.pdf")
-class(policy_text)
-length(policy_text)
-cutter <- worker(bylines = TRUE, symbol = TRUE)
-segment(policy_text, cutter)
-
-fake.text<-'R text mining is good but text mining in Python is also'
-gsub('text mining','tm',fake.text,ignore.case = F)
-sub('text mining','tm',fake.text,ignore.case = F)
-
-# try to extract the policy text
 library(RMeCab)
 library(pdftools)
 library(wordcloud)
 
-policy_text <- pdf_text("Policy.pdf")
+Sys.setlocale(category = "LC_ALL", locale = "Japanese")
+policy_text <- pdf_text("Policy_text/京都市.pdf")
 policy_text[1]
 # 文本显示为日语
 nchar(policy_text)
 # 共两项，一千多字
 
 # 文本处理
-# 先对第一项进行
-policy_text <- policy_text[1]
 class(policy_text)
 length(policy_text)
 # 删除特殊字符
@@ -34,16 +17,16 @@ policy_text <- gsub("\r", "", policy_text)
 policy_text <- gsub("\n", "", policy_text)
 policy_text <- gsub("@", "", policy_text)
 policy_text
+# 删除数字
+
 # 日语分词怎么做？
 # 消除停止词之后直接分析词频吧
 # 先写入该文档
 write(policy_text, "Policy.txt")
 # 写入的文档是ANSI编码，但直接用RMecab分析的话有时会显示乱码？
-Sys.setlocale(category = "LC_ALL", locale = "Japanese")
 policy_freq <- RMeCabFreq("Policy.txt")
 policy_freq
 head(policy_freq)
-tail
 policy_freq_sort <- policy_freq[order(policy_freq$Freq, decreasing = TRUE),]
 head(policy_freq_sort, 20)
 tail(policy_freq_sort)
@@ -54,8 +37,3 @@ policy_freq_sort_non <- subset(policy_freq_sort_non, Info2 != "非自立")
 policy_freq_sort_non <- subset(policy_freq_sort_non, Info2 != "接尾")
 nrow(policy_freq_sort_non)
 head(policy_freq_sort_non, 20)
-
-
-
-
-
