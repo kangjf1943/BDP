e0d4b82d0a64963620f3fabb5a4b4ca2c5356ec6
Jiefeng Kang <kangjf1943@gmail.com>
2021-02-25 21:37:47 +0900

    Optimize transform of pdf to txt and to corpus

--- Data_analysis.R
+++ Data_analysis.R
@@ -1,41 +1,35 @@
 library(RMeCab)
 library(pdftools)
 library(wordcloud)
+library(tm)
 
+# set computer "Language for non-Unicode programs settings" and Japanese without "Use Unicode UTF-8 for worldwide language support": Settings > Region > Additional date, time & regional > Change date, time, or number format > Administrative > Language for non-Unicode programs settings
+Sys.setlocale(category = "LC_ALL", locale = "Japanese")
 setwd("C:/Users/kangj/Documents/R/BDP/Policy_pdf")
 for (i in list.files("C:/Users/kangj/Documents/R/BDP/Policy_pdf")) {
-  Sys.setlocale(category = "LC_ALL", locale = "Japanese")
-  policy_text <- pdf_text(i)
-  # 文本显示为日语吗？
-  nchar(policy_text)
-  
-  # 文本处理
-  class(policy_text)
-  length(policy_text)
-  # 删除特殊字符
-  policy_text <- gsub("\r", "", policy_text)
-  policy_text <- gsub("\n", "", policy_text)
-  policy_text <- gsub("@", "", policy_text)
-  policy_text
-  # 删除数字
-  
-  # 日语分词怎么做？
-  # 消除停止词之后直接分析词频吧
-  # 先写入该文档
-  write(policy_text, paste("C:/Users/kangj/Documents/R/BDP/Policy_text/",i,".txt", sep = ""))
+  Policy_txt <- pdf_text(i)
+  write(Policy_txt, paste("C:/Users/kangj/Documents/R/BDP/Policy_txt/", 
+                           gsub(".pdf", "", i),".txt", sep = ""))
 }
+setwd("C:/Users/kangj/Documents/R/BDP")
 
-# 写入的文档是ANSI编码，但直接用RMecab分析的话有时会显示乱码？
-policy_freq <- RMeCabFreq("Policy.txt")
-policy_freq
-head(policy_freq)
-policy_freq_sort <- policy_freq[order(policy_freq$Freq, decreasing = TRUE),]
-head(policy_freq_sort, 20)
-tail(policy_freq_sort)
-policy_freq_sort_non <- subset(policy_freq_sort, Info1 == "名詞")
-nrow(policy_freq_sort_non)
-head(policy_freq_sort_non, 20)
-policy_freq_sort_non <- subset(policy_freq_sort_non, Info2 != "非自立")
-policy_freq_sort_non <- subset(policy_freq_sort_non, Info2 != "接尾")
-nrow(policy_freq_sort_non)
-head(policy_freq_sort_non, 20)
+# 建立语料库
+policy_corpus <- VCorpus(DirSource("Policy_txt"))
+# 数据清理
+func_clean_corpus <- function(var_corpus){
+  var_corpus <- tm_map(var_corpus, removePunctuation)
+  var_corpus <- tm_map(var_corpus, removeNumbers)
+  # 批量替换特定字符的函数是？
+  # 如何删除停止词？
+  return(var_corpus)
+}
+policy_corpus <- func_clean_corpus(policy_corpus)
+if(!file.exists("Policy_corpus")){dir.create("Policy_corpus")}
+if(length(list.files("Policy_corpus")) != 0)
+  {file.remove(paste0("Policy_corpus/", list.files("Policy_corpus")))}
+writeCorpus(policy_corpus, "Policy_corpus")
+file.rename(from = paste0("Policy_corpus/",list.files("Policy_corpus")),
+            to = paste0("Policy_corpus/",
+                        gsub("txt", "", list.files("Policy_corpus"))))
+
+
